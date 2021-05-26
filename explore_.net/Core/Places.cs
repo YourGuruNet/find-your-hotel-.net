ada912cb08799a19e846ce264a65fc232a600df0
arvis <info@yourgurunet.com>
2021-05-26 20:26:34 +0300

    Created place model

---
+++ explore_.net/Core/Places.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using explore_.net.Models;
+using MediatR;
+
+namespace explore_.net.Core
+{
+    public class Places
+    {
+        public class Query : IRequest<List<Place>>
+        {
+        }
+    }
+}
---
+++ explore_.net/Models/Place.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace explore_.net.Models
+{
+    public class Place
+    {
+
+        public int PlaceId {get; set;}
+        public string Title { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public int CreatorId { get; set; }
+        public string Picture { get; set; }
+
+    }
+}
