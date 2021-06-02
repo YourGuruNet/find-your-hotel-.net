9592b2640488c8b99f305d602c3bd27b48c94b79
arvis <info@yourgurunet.com>
2021-06-02 21:28:32 +0300

    Get Place by id: * GetPlacesById.cs:
    * PlacesController.cs:

--- explore_.net/Controllers/PlacesController.cs
+++ explore_.net/Controllers/PlacesController.cs
@@ -7,11 +7,18 @@ namespace explore_.net.Controllers
     public class PlacesController : BaseApiController
     {
         private static readonly GetPlacesCommand _placesActions = new GetPlacesCommand();
+        private static readonly GetPlacesById _placesById = new GetPlacesById();
 
         [HttpGet]
         public IList<Place> GetPlaces()
         {
             return _placesActions.GetList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Place> GetPlacesById(int id)
+        {
+            return _placesById.GetById(id);
         }
     }
 }
