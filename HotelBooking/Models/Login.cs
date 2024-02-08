946c25370d69b82ab50d4a7030a60df645a9b974
YourGuruNet <arvisiljins@gmail.com>
2024-02-08 14:21:36 +0200

    hide key from swagger

--- HotelBooking/Models/Login.cs
+++ HotelBooking/Models/Login.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using System.Text.Json.Serialization;
+
 namespace HotelBooking.Models
 {
     public class Login
     {
         public string? Email { get; set; }
         public string? Password { get; set; }
+        [JsonIgnore]
         public string? Key { get; set; }
     }
 }
